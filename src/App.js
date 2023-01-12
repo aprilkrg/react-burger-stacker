14e49dc0d93f3b89d4a71308715b2ea7c2ee3913
aprilkrg <aprilkrgonzales@gmail.com>
2023-01-12 12:22:47 -0700

    set up react app with empty app.js for deliverable

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
 import "./App.css";
-import React, { Component } from "react";
-import IngredientList from "./components/IngredientList";
-import BurgerPane from "./components/BurgerPane";
+import { Component } from "react";
 
-const ingredientsArr = [
-    { name: "Kaiser Bun", color: "saddlebrown" },
-    { name: "Sesame Bun", color: "sandybrown" },
-    { name: "Gluten Free Bun", color: "peru" },
-    { name: "Lettuce Wrap", color: "olivedrab" },
-    { name: "Beef Patty", color: "#3F250B" },
-    { name: "Soy Patty", color: "#3F250B" },
-    { name: "Black Bean Patty", color: "#3F250B" },
-    { name: "Chicken Patty", color: "burlywood" },
-    { name: "Lettuce", color: "lawngreen" },
-    { name: "Tomato", color: "tomato" },
-    { name: "Bacon", color: "maroon" },
-    { name: "Onion", color: "lightyellow" },
-];
-
-class App extends Component {
-	state = {
-		stack: []
-	}
-
-	handleAddToStack = (e) => {
-		console.log("add to stack", e.target.innerText, e.target.style.backgroundColor)
-		const newStateIngredient = {
-			name: e.target.innerText,
-			color: e.target.style.backgroundColor
-		}
-		const stateCopy = this.state.stack
-		stateCopy.push(newStateIngredient)
-		this.setState({
-			stack: stateCopy
-		})
-	}
-
-	handleRemoveFromStack = (e) => {
-		console.log("remove from stack", this.state.stack, 'STATE <<<')
-		this.setState({
-			stack: []
-		})
-	}
+export default class App extends Component {
 	render() {
         return (
 			<>
 				<h1>Burger Stacker</h1>
-				<div className="list-div">
-					<IngredientList 
-						items={ingredientsArr} 
-						handleAddToStack={this.handleAddToStack}
-					/>
-				</div>
-				<div className="stack-div">
-					<BurgerPane 
-						stack={this.state.stack}
-						handleRemoveFromStack={this.handleRemoveFromStack}
-					/>
-				</div>
 			</>
         );
     }
 }
-
-export default App
