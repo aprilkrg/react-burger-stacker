94114ac5ea84892e27a3e6f5cc4ff7d8222487ec
aprilkrg <aprilkrgonzales@gmail.com>
2023-01-12 17:50:37 -0700

    center submit button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,23 +4,32 @@ export default class Input extends Component {
     render() {
         return (
             <>
-                <form onSubmit={this.props.handleAddOne}>
-                    <label htmlFor='ingredInput'>Add New Ingredient: </label>
+                <form 
+                    onSubmit={this.props.handleAddOne}
+                >
+                    <label htmlFor='ingredInput'>New Ingredient: </label>
                     
                     <input
                         type='text'
                         id='ingredInput'
-                        placeholder='add a new ingredient to the list'
+                        placeholder=' add a new ingredient'
                         onChange={this.props.handleChange}
                         value={this.props.input}
-                    />
-                    <input type="submit" value="Submit" 
+                        />
+                    <div
                         className="
+                            flex justify-center
+                        "
+                    >
+
+                        <input type="submit" value="Submit" 
+                            className="
                             p-1
                             bg-green-300
                             border-4 border-green-400
-                        "
-                    />
+                            "
+                            />
+                    </div>
                 </form>
             </>
         )
