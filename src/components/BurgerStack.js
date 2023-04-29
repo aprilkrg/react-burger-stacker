e48c4c01565a05faacaad4a50cc69061ddec7fba
dxk0ta <dxkotacamps@gmail.com>
2023-04-29 13:30:26 -0700

    touching up and turning in

---
+++ src/components/BurgerStack.js
@@ -0,0 +1,21 @@
+
+ 
+
+import { Component } from 'react'
+import Ingredient from './Ingredient'
+
+export default class BurgerStack extends Component {
+    render() {
+        const stackToRender = this.props.stack.map((item, i) => {
+            return <Ingredient 
+                key={"stack-" + i}
+                item={item}
+            />
+        })
+        return(
+            <>
+                {stackToRender.reverse()}
+            </>
+        )
+    }
+}
